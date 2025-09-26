d33250b17dad1ce61313e319bb780fa2db4af105
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-09-26 10:22:19 +0530

    AZVMSTARTSTOP

--- startstopvm.ps1
+++ startstopvm.ps1
@@ -7,12 +7,12 @@ $VM_NAME = "azuretest"
 
 if ($Action -eq "Start") {
     # Start-AzVM -ResourceGroupName $rgName -Name $vmName
-    az vm start --resource-group "$(RESOURCE_GROUP)" --name "$(VM_NAME)"
+    az vm start --resource-group "azuretest_rg" --name "azuretest"
     Write-Host "VM started successfully"
 }
 elseif ($Action -eq "Stop") {
     # Stop-AzVM -ResourceGroupName $rgName -Name $vmName -Force
-    az vm deallocate --resource-group "$(RESOURCE_GROUP)" --name "$(VM_NAME)"
+    az vm deallocate --resource-group "azuretest_rg" --name "azuretest"
     Write-Host "VM stopped successfully"
 }
 else {
